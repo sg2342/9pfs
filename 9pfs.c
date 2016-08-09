e3d5bf356597f42411d0d0825cf2b3156b1ff7d6
Stefan Grundmann <sg2342@googlemail.com>
2016-08-09 22:28:00 +0000

    fix: adding 'off_t' (aka 'long') to a string does not append to the string [-Wstring-plus-int]

--- 9pfs.c
+++ 9pfs.c
@@ -254,7 +254,7 @@ fsread(const char *path, char *buf, size_t size, off_t off,
 		size = CACHECTLSIZE;
 		if(off >= size)
 			return 0;
-		memcpy(buf, "cleared\n" + off, size - off);
+		memcpy(buf, &"cleared\n"[off], size - off);
 		clearcache(path);
 		return size;
 	}
